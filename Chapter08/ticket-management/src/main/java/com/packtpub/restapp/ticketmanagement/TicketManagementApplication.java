9525e911bf9f3329ea93f33979d7d11620dea355
rajacsp <raja.csp@gmail.com>
2018-01-14 22:58:10 -0500

    Unnecessary code removed

--- Chapter08/ticket-management/src/main/java/com/packtpub/restapp/ticketmanagement/TicketManagementApplication.java
+++ Chapter08/ticket-management/src/main/java/com/packtpub/restapp/ticketmanagement/TicketManagementApplication.java
@@ -1,12 +1,12 @@
 package com.packtpub.restapp.ticketmanagement;
 
 import javax.servlet.Filter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @ComponentScan("com.packtpub")
