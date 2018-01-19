2bc6c5726d4f3f24acc9b75fa1131ae9f152c142
rajacsp <raja.csp@gmail.com>
2018-01-19 15:10:49 -0500

    Missing Error handler added in Ch 10

--- Chapter10/ticket-management/src/main/java/com/packtpub/restapp/HomeController.java
+++ Chapter10/ticket-management/src/main/java/com/packtpub/restapp/HomeController.java
@@ -16,68 +16,78 @@ import com.packtpub.service.SecurityService;
 
 @RestController
 @RequestMapping("/")
-public class HomeController {
-	
+public class HomeController extends ErrorHandler {
+
 	private static final Logger _logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	@Autowired
 	SecurityService securityService;
-	
+
 	@ResponseBody
 	@RequestMapping("")
 	public Map<String, Object> test() {
 		Map<String, Object> map = new LinkedHashMap<>();
 		map.put("result", "Aloha");
-		
+
 		_logger.trace("{test} trace");
 		_logger.debug("{test} debug");
 		_logger.info("{test} info");
 		_logger.warn("{test} warn ");
 		_logger.error("{test} error");
-		
+
 		return map;
 	}
-	
+
 	@ResponseBody
 	@RequestMapping("/test/aop/with/annotation")
 	@TokenRequired
-	public Map<String, Object> testAOPAnnotation(){
+	public Map<String, Object> testAOPAnnotation() {
 		Map<String, Object> map = new LinkedHashMap<>();
 		map.put("result", "Aloha");
-		
+
 		return map;
 	}
-	
+
 	@ResponseBody
-	@RequestMapping("/test/aop/with/execution")	
-	public Map<String, Object> testAOPExecution(){
+	@RequestMapping("/test/aop/with/execution")
+	public Map<String, Object> testAOPExecution() {
 		Map<String, Object> map = new LinkedHashMap<>();
 		map.put("result", "Aloha");
-		
+
 		return map;
 	}
-	
+
 	@ResponseBody
 	@RequestMapping("/security/generate/token")
-	public Map<String, Object> generateToken(@RequestParam(value="subject") String subject){
-		
+	public Map<String, Object> generateToken(@RequestParam(value = "subject") String subject) {
+
 		String token = securityService.createToken(subject, (15 * 1000 * 60));
-		
+
 		Map<String, Object> map = new LinkedHashMap<>();
 		map.put("result", token);
-		
+
 		return map;
 	}
-	
+
 	@ResponseBody
 	@RequestMapping("/security/get/subject")
-	public Map<String, Object> getSubject(@RequestParam(value="token") String token){
-		
+	public Map<String, Object> getSubject(@RequestParam(value = "token") String token) {
+
 		String subject = securityService.getSubject(token);
-		
+
 		Map<String, Object> map = new LinkedHashMap<>();
 		map.put("result", subject);
-		
+
 		return map;
 	}
+
+	// other methods
+	@ResponseBody
+	@RequestMapping("/test/error")
+	public Map<String, Object> testError(@RequestParam(value = "item") String item) {
+		Map<String, Object> map = new LinkedHashMap<>();
+		map.put("item", item);
+		return map;
+	}
+
 }
