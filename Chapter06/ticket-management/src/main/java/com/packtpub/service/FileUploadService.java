7647dea7ce17bf7651e9db7a2b7fb0a4c585c830
rajacsp <raja.csp@gmail.com>
2018-01-21 22:30:04 -0500

    Ch 06 - Unnecessary files removed

--- Chapter06/ticket-management/src/main/java/com/packtpub/service/FileUploadService.java
+++
@@ -1,9 +0,0 @@
-package com.packtpub.service;
-
-import java.io.IOException;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface FileUploadService {
-	void uploadFile(MultipartFile file) throws IOException;
-}
